8f5ceb8ee4900e42a1c91c5f36eb2658210fc5a8
sbmaxx <sbmaxx@yandex-team.ru>
2018-05-12 11:18:36 +0300

    Build & deploy for pull requests

--- server.js
+++ server.js
@@ -1,61 +1,30 @@
 const express = require('express');
 const app = module.exports = express();
 const rp = require('request-promise');
-const cheerio = require('cheerio')
-const glob = require('glob');
-const path = require('path');
-const qs = require('querystring');
-const yaml = require('js-yaml');
+const cheerio = require('cheerio');
 const { URL } = require('url');
 const chalk = require('chalk');
 const fs = require('fs');
-const postcss = require('./lib/postcss');
+const path = require('path');
+const qs = require('querystring');
+
+const getHostCSS = require('./lib/server/get-host-css');
 
 app.get('/', (req, res) => {
     require('fs').createReadStream('public/index.html').pipe(res);
 });
 
 /**
- *
+ **
  * @param url
  * @returns {string}
  */
 function getHostname(url) {
     try {
         return new URL(url).origin;
-    } catch(e) {
+    } catch (e) {
         return '';
     }
-}
-
-/**
- *
- * @param {string} origin
- * @return {Promise}
- */
-function getHostCSS(origin) {
-    const hosts = glob.sync('hosts/*/HOSTS.yaml').map(file => {
-        try {
-            return {
-                hosts: yaml.safeLoad(fs.readFileSync(file, 'utf8')),
-                style: path.resolve(path.join(path.dirname(file), 'style.css'))
-            };
-        } catch(e) {}
-    }).filter(meta => {
-        if (!meta) {
-            return false;
-        }
-
-        return fs.existsSync(meta.style);
-    });
-
-    const host = hosts.find(host => host.hosts.includes(origin));
-
-    if (!host) {
-        return Promise.resolve('');
-    }
-
-    return postcss(host.style);
 }
 
 function normalize(str) {
@@ -66,7 +35,7 @@ function normalize(str) {
         } else {
             return url;
         }
-    } catch(e) {
+    } catch (e) {
         return '';
     }
 }
@@ -82,33 +51,68 @@ function cleanupParams(queryParams) {
     return params;
 }
 
-function getTurbo(req, query, params) {
+function getTurbo(req, url, params) {
     const headers = { ...req.headers };
     delete headers.host;
 
-    const paramsString = qs.stringify(params);
+    const queryString = qs.stringify(params);
 
     return rp({
-        uri: query ? `https://yandex.ru/turbo?text=${query}${paramsString ? `&${paramsString}` : '' }` : `https://yandex.ru/turbo`,
+        uri: url ?
+            `https://yandex.ru/turbo?text=${url}${queryString ? `&${queryString}` : '' }` :
+            'https://yandex.ru/turbo',
         headers,
         gzip: true
     });
 }
 
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+    const url = normalize(req.query.text);
+
+    req.ctx = {
+        url,
+        hostname: getHostname(url)
+    };
+
+    next();
+});
+
+if (process.env.NODE_ENV !== 'development') {
+    app.use((req, res, next) => {
+        const match = req.hostname.match(/^(.+)\.turboext\.net$/);
+        let pullRequest;
+
+        if (match && fs.existsSync(path.join('checkout', match[1]))) {
+            pullRequest = match[1];
+        } else {
+            return res.redirect('https://master.turboext.net');
+        }
+
+        req.ctx.pullRequest = pullRequest;
+
+        next();
+    });
+}
+
 app.get('/turbo', (req, res, next) => {
-    const query = normalize(req.query.text);
-    const hostname = getHostname(query);
+    const {
+        url,
+        hostname
+    } = req.ctx;
+
     const params = cleanupParams(req.query);
 
     if (!hostname) {
-        return getTurbo(req, query, params).then(html => res.send(html)).catch(e => next(e));
+        return getTurbo(req, url, params).then(html => res.send(html)).catch(e => next(e));
     }
 
     if (params.ajax_type) {
-        return getTurbo(req, query, params).then(html => res.send(html)).catch(e => next(e));
+        return getTurbo(req, url, params).then(html => res.send(html)).catch(e => next(e));
     }
 
-    Promise.all([getTurbo(req, query, params), getHostCSS(hostname)]).then(([html, style]) => {
+    Promise.all([getTurbo(req, url, params), getHostCSS(req, hostname)]).then(([html, style]) => {
         const $ = cheerio.load(html);
 
         $('meta[http-equiv=Content-Security-Policy]').remove();
@@ -118,9 +122,14 @@ app.get('/turbo', (req, res, next) => {
     }).catch(e => next(e));
 });
 
+app.use((req, res) => {
+    res.status(404);
+    res.end('Unknown route');
+});
+
 app.use((err, req, res) => {
     res.status(500);
-    res.send(err);
+    res.end(err);
 });
 
 app.listen(3000, () => {
