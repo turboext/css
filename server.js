497e57a577bc62fd81335a0da8a827d8d92151ff
sbmaxx <sbmaxx@yandex-team.ru>
2018-05-17 01:09:25 +0300

    LiveReload, Ngrok, Frame

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+if (!process.env.NODE_ENV) {
+    process.env.NODE_ENV = 'development';
+}
+
 const express = require('express');
 const app = module.exports = express();
 const rp = require('request-promise');
@@ -7,18 +11,123 @@ const fs = require('fs');
 const path = require('path');
 const qs = require('querystring');
 const compression = require('compression');
-const removeCSP = require('./lib/server/remove-csp');
-const replaceCustomCSS = require('./lib/server/replace-custom-css');
 
-if (!process.env.NODE_ENV) {
-    process.env.NODE_ENV = 'development';
+const removeCSP = require('./lib/server/remove-csp');
+const inject = require('./lib/server/inject');
+
+const styleMiddlware = require('./lib/server/style-middleware');
+
+if (process.env.NODE_ENV !== 'development') {
+    app.use((req, res, next) => {
+        const match = req.hostname.match(/^(.+)\.turboext\.net$/);
+        let pullRequest;
+
+        if (match && fs.existsSync(path.join('checkout', match[1]))) {
+            pullRequest = match[1];
+        } else {
+            return res.redirect('https://master.turboext.net');
+        }
+
+        req.ctx = req.ctx || {};
+        req.ctx.pullRequest = pullRequest;
+
+        next();
+    });
 }
 
-const getHostCSS = require('./lib/server/get-host-css');
+app.use(styleMiddlware);
+app.use(express.static('public'));
+app.use('/hosts', express.static('hosts'));
 
-app.get('/', (req, res) => {
-    require('fs').createReadStream('public/index.html').pipe(res);
+app.use(compression());
+
+app.use((req, res, next) => {
+    const url = normalize(req.query.text);
+
+    req.ctx = req.ctx || {};
+    req.ctx.url = url;
+    req.ctx.hostname = getHostname(url);
+
+    next();
 });
+
+app.get('/turbo', (req, res, next) => {
+    const {
+        url,
+        hostname
+    } = req.ctx;
+
+    const params = cleanupParams(req.query);
+
+    if (!hostname) {
+        return getTurbo(req, url, params).then(html => res.send(html)).catch(e => next(e));
+    }
+
+    if (params.ajax_type) {
+        return getTurbo(req, url, params).then(html => res.send(html)).catch(e => next(e));
+    }
+
+    getTurbo(req, url, params)
+        .then(html => inject(req, html, hostname))
+        .then(html => res.send(html))
+        .catch(e => next(e));
+});
+
+app.get('/frame', (req, res, next) => {
+    fs.readFile('public/frame.html', 'utf8', (e, html) => {
+        if (e) {
+            return next(e);
+        }
+
+        res.send(html.replace(
+            '/turbo?placeholder=1',
+            req.originalUrl.replace('/frame', '/turbo')
+        ));
+    });
+});
+
+app.use((req, res) => {
+    res.status(404);
+    res.end('Unknown route');
+});
+
+app.use((err, req, res) => {
+    res.status(500);
+    res.end(err);
+});
+
+const DEV_SERVER_PORT = 3000;
+const LIVRELOAD_PORT = 35729;
+const PUBLIC = process.env.PUBLIC === 'true';
+
+console.log(`NODE_ENV=${process.env.NODE_ENV}, LIVRELOAD=${process.env.LIVERELOAD}, PUBLIC=${process.env.PUBLIC}`);
+
+app.listen(DEV_SERVER_PORT, () => {
+    if (PUBLIC) {
+        const ngrok = require('ngrok');
+
+        ngrok.connect(DEV_SERVER_PORT).then(url => {
+            console.log(`DevServer started at ${chalk.blue.underline(`${url}`)}`);
+        }).catch(e => {
+            console.error(e);
+            process.exit(1);
+        });
+    } else {
+        console.log(`DevServer started at ${chalk.blue.underline(`http://localhost:${DEV_SERVER_PORT}`)}`);
+    }
+});
+
+if (process.env.LIVERELOAD === 'true' && process.env.NODE_ENV !== 'production') {
+    if (PUBLIC) {
+        console.log('Livereload has beed disabled due public mode');
+        process.env.LIVERELOAD = 'false';
+    } else {
+        const livereload = require('./lib/server/lr');
+        livereload().listen(LIVRELOAD_PORT, () => {
+            console.log(`LiveReload started at ${chalk.blue.underline(`http://localhost:${LIVRELOAD_PORT}`)}`);
+        });
+    }
+}
 
 /**
  **
@@ -69,75 +178,5 @@ function getTurbo(req, url, params) {
             'https://yandex.ru/turbo',
         headers,
         gzip: true
-    });
+    }).then(removeCSP);
 }
-
-if (process.env.NODE_ENV !== 'development') {
-    app.use((req, res, next) => {
-        const match = req.hostname.match(/^(.+)\.turboext\.net$/);
-        let pullRequest;
-
-        if (match && fs.existsSync(path.join('checkout', match[1]))) {
-            pullRequest = match[1];
-        } else {
-            return res.redirect('https://master.turboext.net');
-        }
-
-        req.ctx = req.ctx || {};
-        req.ctx.pullRequest = pullRequest;
-
-        next();
-    });
-}
-
-app.use(express.static('public'));
-
-app.use(compression());
-
-app.use((req, res, next) => {
-    const url = normalize(req.query.text);
-
-    req.ctx = req.ctx || {};
-    req.ctx.url = url;
-    req.ctx.hostname = getHostname(url);
-
-    next();
-});
-
-app.get('/turbo', (req, res, next) => {
-    const {
-        url,
-        hostname
-    } = req.ctx;
-
-    const params = cleanupParams(req.query);
-
-    if (!hostname) {
-        return getTurbo(req, url, params).then(html => res.send(html)).catch(e => next(e));
-    }
-
-    if (params.ajax_type) {
-        return getTurbo(req, url, params).then(html => res.send(html)).catch(e => next(e));
-    }
-
-    Promise.all([getTurbo(req, url, params), getHostCSS(req, hostname)]).then(([rawHtml, style]) => {
-        let html = removeCSP(rawHtml);
-        html = replaceCustomCSS(html, style);
-
-        res.send(html);
-    }).catch(e => next(e));
-});
-
-app.use((req, res) => {
-    res.status(404);
-    res.end('Unknown route');
-});
-
-app.use((err, req, res) => {
-    res.status(500);
-    res.end(err);
-});
-
-app.listen(3000, () => {
-    console.log(`DevServer started at ${chalk.blue.underline('http://localhost:3000')}. [${process.env.NODE_ENV}]`);
-});
--- test/replace-custom-css.spec.js
+++ test/replace-custom-css.spec.js
@@ -1,17 +1,18 @@
 const replaceCSS = require('../lib/server/replace-custom-css');
+const removeCSS = require('../lib/server/remove-custom-css');
 const { describe, it } = require('mocha');
 const assert = require('chai').assert;
 
-describe('replace css', () => {
+describe('remove&replace css', () => {
     const html = '<html><head><style>.body { color: black; }</style><style data-name=custom>.body { color: blue; }</style></head><body></body></html>';
 
     it('should remove old styles html string', () => {
-        const htmlWithoutCSS = '<html><head><style>.body { color: black; }</style></head><body></body></html>';
-        assert.equal(replaceCSS(html), htmlWithoutCSS);
+        const htmlWithoutCSS = '<html><head><style>.body { color: black; }</style><!--custom-css-placeholder--></head><body></body></html>';
+        assert.equal(removeCSS(html), htmlWithoutCSS);
     });
-    
+
     it('should replace old styles with new one', () => {
         const htmlWithNewCSS = '<html><head><style>.body { color: black; }</style><style data-name=custom>.page { color: pink; }</style></head><body></body></html>';
-        assert.equal(replaceCSS(html, '.page { color: pink; }'), htmlWithNewCSS);
+        assert.equal(replaceCSS(html, '<style data-name=custom>.page { color: pink; }</style>'), htmlWithNewCSS);
     });
 });
