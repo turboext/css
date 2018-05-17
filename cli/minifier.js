1b8dfed3ec0bab48611391b2cd85a0329f427067
sbmaxx <sbmaxx@yandex-team.ru>
2018-05-18 02:38:10 +0300

    Added callback for async function

--- cli/minifier.js
+++ cli/minifier.js
@@ -5,5 +5,5 @@ const files = glob.sync('./hosts/**/*.css');
 
 files.forEach(from => {
     const to = from.replace(/\.css$/, '.min.css');
-    postcss(from, to).then(result => fs.writeFile(to, result));
+    postcss(from, to).then(result => fs.writeFile(to, result, () => void 0));
 });
