00ecd4dfa69185bbea89a295878fdd5f97a310fc
sbmaxx <sbmaxx@yandex-team.ru>
2018-07-26 11:15:28 +0300

    Queue minify process

--- cli/minifier.js
+++ cli/minifier.js
@@ -2,8 +2,26 @@ const postcss = require('../lib/postcss');
 const glob = require('glob');
 const fs = require('fs');
 const files = glob.sync('./hosts/**/style.*').filter(file => !file.endsWith('min.css'));
+const async = require('async');
+const queue = async.queue(async ({ from, to }, callback) => {
+    process.stdout.write(`Minify ${from}...`);
 
-files.forEach(from => {
+    try {
+        const result = await postcss(from, to);
+        fs.writeFileSync(to, result);
+        process.stdout.write('ok');
+    } catch(e) {
+        process.stdout.write('failed');
+        console.log(e);
+    }
+    console.log('');
+
+    callback();
+}, 1);
+
+queue.drain = () => console.log('All items has been uploaded');
+
+queue.push(files.map(from => {
     const to = from.replace(/\.s?css$/, '.min.css');
-    postcss(from, to).then(result => fs.writeFile(to, result, () => void 0));
-});
+    return { from, to };
+}));
